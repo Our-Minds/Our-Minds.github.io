9efd913cfef1c3481d14c99b0acff2e5af9b4eec
BrianxBorne <brianxborne@gmail.com>
2025-04-29 14:01:10 +0300

    working dev server

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // Remove BrowserRouter here
 import { AuthProvider } from "./context/AuthContext";
 
 // Pages
@@ -26,22 +25,20 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/chat" element={<ChatPage />} />
-            <Route path="/chat/:consultantId" element={<ChatPage />} />
-            <Route path="/consult" element={<ConsultPage />} />
-            <Route path="/book-session" element={<BookSessionPage />} />
-            <Route path="/book-session/:consultantId" element={<BookSessionPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/admin" element={<AdminPanel />} />
-            <Route path="/story/:storyId" element={<StoryPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <Routes> {/* Removed BrowserRouter here */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/chat/:consultantId" element={<ChatPage />} />
+          <Route path="/consult" element={<ConsultPage />} />
+          <Route path="/book-session" element={<BookSessionPage />} />
+          <Route path="/book-session/:consultantId" element={<BookSessionPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/admin" element={<AdminPanel />} />
+          <Route path="/story/:storyId" element={<StoryPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </TooltipProvider>
     </AuthProvider>
   </QueryClientProvider>
